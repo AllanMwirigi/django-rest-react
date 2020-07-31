6a7417b95ec98e42a89d5f6948aaac2887e9599f
AllanMwirigi <allo.mwirigi@gmail.com>
2020-07-31 17:32:17 +0300

    deployment fixes

---
+++ frontend/build/precache-manifest.351c1ff968a2a8f6fff89e68e4297c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a54832507e8351f164bbf8cbd054d03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1d41caeb53f3a3d79a",
+    "url": "/static/css/main.896a08b1.chunk.css"
+  },
+  {
+    "revision": "482bca71368e8a3de53d",
+    "url": "/static/js/2.57babf8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.57babf8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1d41caeb53f3a3d79a",
+    "url": "/static/js/main.01a11e69.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
